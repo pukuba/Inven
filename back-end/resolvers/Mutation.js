bb5c27c5af667534f1323868679298485c458863
pukuba <skatmddnjs0604@gmail.com>
2020-07-12 17:06:32 +0900

    회원가입시 최소길이 업데이트
    ㅈㄱㄴ^^7

--- back-end/resolvers/Mutation.js
+++ back-end/resolvers/Mutation.js
@@ -8,9 +8,9 @@ module.exports = {
     join: async(parent,args,{ db }) => {
         const check1 = await db.collection('user').findOne({id:args.id})
         const check2 = await db.collection('user').findOne({name:args.name})
-        if(check1 != null || !stringCheck(args.id)) return "id에 오류가 있습니다."
-        if(check2 != null || !stringCheck(args.name)) return "name에 오류가 있습니다."
-        if(args.pw < 7) return "pw에 오류가 있습니다."
+        if(check1 != null || !stringCheck(args.id) || args.id.length < 6) return "id에 오류가 있습니다."
+        if(check2 != null || !stringCheck(args.name) || args.name.length < 6) return "name에 오류가 있습니다."
+        if(args.pw.length < 6) return "pw에 오류가 있습니다."
         let seed = Math.round((new Date().valueOf() * Math.random())) + "";
         const user = [{
             id:args.id,
