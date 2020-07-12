de07144db27cb017bd0bc27331e0d83bdc62ffb9
WonDongGyu <windowdong11@naver.com>
2020-07-12 14:42:28 +0900

    Add login, signup styles

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import './login.css';
+import logo from './../logo.svg';
+/*
+<div className="login-wrapper">
+      <form className="login-form">
+        <p>Login</p>
+        <div>
+          <input type="text" id="login-id" placeholder="ID"/>
+        </div>
+        <div>
+        <input type="password" id="login-password" placeholder="Password"/>
+        </div>
+        <div>
+        <button type="submit" id="login-button">login</button>
+        </div>
+      </form>
+    </div>
+*/
 
 function LoginForm() {
+  // from https://www.w3schools.com/howto/howto_css_login_form.asp
     return (
-    <form>
-          <fieldset className="login_form">
-            <legend>Login</legend>
-            <input type="text" id="login-id" placeholder="ID"/>
-            <input type="password" id="login-password" placeholder="Password"/>
-            <button type="submit" id="login-button">login</button>
-          </fieldset>
-    </form>
+    <form method="post">
+    <div class="imgcontainer">
+      <img src={logo} className="App-logo" alt="logo"/>
+    </div>
+  
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required/>
+  
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required/>
+  
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"/> Remember me
+      </label>
+    </div>
+  
+    <div class="container bottom-buttons-container">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="/">password?</a></span>
+    </div>
+  </form>
     );
 }
 export default LoginForm;
--- front-end/src/pages/signup.js
+++ front-end/src/pages/signup.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import './signup.css';
 
+/*
+<div className="signup-wrapper">
+        <form className="signup-form">
+          <p>SignUp</p>
+          <label>ID</label>
+          <input type="text" id="user_id" />
+          <br/>
+          <label>Password</label>
+          <input type="password" id="password" />
+          <br/>
+          <button id="submitBtn" type="submit">Submit</button>
+        </form>
+      </div>
+*/
+
 function SignUpForm() {
+  // from https://www.w3schools.com/howto/howto_css_signup_form.asp
     return (
-      <form>
-            <fieldset>
-              <legend>Register</legend>
-              <label>ID</label>
-              <input type="text" id="user_id" />
-              <br/>
-              <label>Password</label>
-              <input type="password" id="password" />
-              <br/>
-              <button id="submitBtn" type="submit">
-                <span>submit</span>
-              </button>
-            </fieldset>
-      </form>
+      <form >
+      <div class="container">
+        <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account.</p>
+        <hr/>
+    
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required/>
+    
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required/>
+    
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
+    
+        <label>
+          <input type="checkbox" name="remember"/> Remember me
+        </label>
+    
+        {/* <p>By creating an account you agree to our <a href="/">Terms & Privacy</a>.</p> */}
+    
+        <div class="clearfix">
+          <button type="button" class="cancelbtn">Cancel</button>
+          <button type="submit" class="signupbtn">Sign Up</button>
+        </div>
+      </div>
+    </form>
       );
   }
 
