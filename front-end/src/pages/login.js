33be85bcecf49de80872637c32f9ccde0f4106b1
WonDongGyu <windowdong11@naver.com>
2020-07-12 15:19:42 +0900

    로그인 화면 remember me 버튼 checked 옵션 제거
    체크 비활성화가 안되는 오류 해결

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -35,7 +35,7 @@ function LoginForm() {
   
       <button type="submit">Login</button>
       <label>
-        <input type="checkbox" checked="checked" name="remember"/> Remember me
+        <input type="checkbox" name="remember"/> Remember me
       </label>
     </div>
   
