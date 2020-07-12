6268f1a877fa8309c35b66fa1f41233ecc31483a
pukuba <skatmddnjs0604@gmail.com>
2020-07-12 15:39:35 +0900

    회원가입 로직 변경
    ㅈㄱㄴ

--- back-end/resolvers/Mutation.js
+++ back-end/resolvers/Mutation.js
@@ -2,9 +2,15 @@ const crypto = require('crypto');
 
 const makePw = (x,y) => crypto.createHash("sha512").update(x + y).digest("hex");
 
+const stringCheck = (x) => ('0' <= x && x <= '9' || 'a' <= x && x <= 'z' || 'A' <= x && x <= 'Z') 
+
 module.exports = {
     join: async(parent,args,{ db }) => {
-        if(!args.check) return false
+        const check1 = await db.collection('user').findOne({title:args.id})
+        const check2 = await db.collection('user').findOne({title:args.name})
+        if(check1 != null || stringCheck(args.id)) return "id에 오류가 있습니다."
+        if(check2 != null || stringCheck(args.name)) return "name에 오류가 있습니다."
+        if(args.pw < 7) return "pw에 오류가 있습니다."
         let seed = Math.round((new Date().valueOf() * Math.random())) + "";
         const user = [{
             id:args.id,
@@ -16,7 +22,7 @@ module.exports = {
             seed:seed
         }]
         await db.collection('Inven').insertMany(user)
-        return true
+        return "true"
     }
 
 }
