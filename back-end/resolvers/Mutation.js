7477263a108fe7f70e6c79129f928733f2198e34
pukuba <skatmddnjs0604@gmail.com>
2020-07-16 12:40:59 +0900

    update login logic

--- back-end/resolvers/Mutation.js
+++ back-end/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const { SSL_OP_NETSCAPE_CA_DN_BUG } = require('constants');
 require('date-utils')
 const makePw = (x,y) => crypto.createHash("sha512").update(x + y).digest("hex");
 
@@ -10,7 +11,7 @@ module.exports = {
         if(id != null || !stringCheck(args.id) || args.id.length < 6) return "id에 오류가 있습니다."
         if(name != null || !stringCheck(args.name) || args.name.length < 6) return "name에 오류가 있습니다."
         if(args.pw.length < 6) return "pw에 오류가 있습니다."
-        let seed = Math.round((new Date().valueOf() * Math.random())) + "";
+        let seed = Math.round((new Date().valueOf() * Math.random())) + "",newDate = new Date();
         const user = [{
             id:args.id,
             pw:makePw(args.pw,seed),
@@ -20,7 +21,8 @@ module.exports = {
             exp:0,
             icon:1,
             seed:seed,
-            token:null
+            token:null,
+            date: newDate.toFormat('YYYY-MM-DD HH24:MI:SS')
         }]
         await db.collection('user').insertMany(user)
         return "true"
@@ -28,10 +30,21 @@ module.exports = {
 
     login: async(parent, args,{ db,token }) => {
         const id = args.id, pw = args.pw, info = await db.collection('user').findOne({id:args.id})
+        let newDate = new Date(),time = newDate.toFormat('YYYY-MM-DD HH24:MI:SS')
         if(info === null) return false
         if(args.id == info.id && info.pw == makePw(args.pw,info.seed)){
             let seed = Math.round((new Date().valueOf() * Math.random())) + "";
             await db.collection('user').update({id:args.id},{$set:{'token':seed}})
+            if(time[8]+time[9] != info.latest[8]+info.latest[9]){
+                await db.collection('user').update({id:args.id},{$set:{'latest' : time}})
+                await db.collection('user').udpate({id:args.id},{$set:{'exp':info.exp+50}})
+                info = await db.collection('user').findOne({id:args.id})
+                ep = Math.round(100 * Math.pow(1.5,info.level-1))
+                if(info.exp >= ep){
+                    await db.collection('user').update({id:args.id},{$set:{'level' : info.level+1}})
+                    await db.collection('user').update({id:args.id},{$set:{'exp' : exp-ep}})
+                }
+            }
             return seed
         }
         return null
@@ -52,7 +65,7 @@ module.exports = {
             content: args.content,
             author: user.name,
             date: newDate.toFormat('YYYY-MM-DD HH24:MI:SS'),
-            type:args.type
+            type: args.type
         }]
         await db.collection('post').insertMany(posts) 
         return true
