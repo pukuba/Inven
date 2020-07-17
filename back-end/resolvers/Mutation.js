6075d2611075c4847798bf696ded59b1e8bfa368
pukuba <skatmddnjs0604@gmail.com>
2020-07-17 22:36:34 +0900

    Update Mutation.js

--- back-end/resolvers/Mutation.js
+++ back-end/resolvers/Mutation.js
@@ -3,11 +3,16 @@ const { SSL_OP_NETSCAPE_CA_DN_BUG } = require('constants');
 require('date-utils')
 const makePw = (x,y) => crypto.createHash("sha512").update(x + y).digest("hex");
 
-const stringCheck = (x) => ('0' <= x && x <= '9' || 'a' <= x && x <= 'z' || 'A' <= x && x <= 'Z') 
+const stringCheck = (x) => {
+    let go = 0;
+    for(let i=0; i<x.length; i++) if(('0' <= x[i] && x[i] <= '9') || ('a' <= x[i] && x[i] <= 'z') || ('A' <= x[i] && x[i] <= 'Z')) go++
+    return go == x.length
+} 
 
 module.exports = {
     join: async(parent, args,{ db,session }) => {
         const id = await db.collection('user').findOne({id:args.id}), name = await db.collection('user').findOne({name:args.name})
+        console.log(id)
         if(id != null || !stringCheck(args.id) || args.id.length < 6) return "id에 오류가 있습니다."
         if(name != null || !stringCheck(args.name) || args.name.length < 6) return "name에 오류가 있습니다."
         if(args.pw.length < 6) return "pw에 오류가 있습니다."
@@ -22,7 +27,7 @@ module.exports = {
             icon:1,
             seed:seed,
             token:null,
-            date: newDate.toFormat('YYYY-MM-DD HH24:MI:SS')
+            latest: newDate.toFormat('YYYY-MM-DD HH24:MI:SS')
         }]
         await db.collection('user').insertMany(user)
         return "true"
@@ -33,8 +38,8 @@ module.exports = {
         let newDate = new Date(),time = newDate.toFormat('YYYY-MM-DD HH24:MI:SS')
         if(info === null) return false
         if(args.id == info.id && info.pw == makePw(args.pw,info.seed)){
-            let seed = Math.round((new Date().valueOf() * Math.random())) + "";
-            await db.collection('user').update({id:args.id},{$set:{'token':seed}})
+            let token = Math.round((new Date().valueOf() * Math.random())) + "";
+            await db.collection('user').update({id:args.id},{$set:{'token':token}})
             if(time[8]+time[9] != info.latest[8]+info.latest[9]){
                 await db.collection('user').update({id:args.id},{$set:{'latest' : time}})
                 await db.collection('user').udpate({id:args.id},{$set:{'exp':info.exp+50}})
@@ -45,7 +50,7 @@ module.exports = {
                     await db.collection('user').update({id:args.id},{$set:{'exp' : exp-ep}})
                 }
             }
-            return seed
+            return token
         }
         return null
     },
